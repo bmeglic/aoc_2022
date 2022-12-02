1f808277e9f5f8b8ebf37d587805f9121cd9ee47
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2022-12-02 19:03:23 +0100

    [aoc] Add template files

---
+++ template/src/aoc.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn run(file: String) {
+    if let Ok(lines) = read_lines(&file) {
+        for line in lines {
+            if let Ok(line) = line {}
+        }
+    } else {
+        println!("Could not open/read file: {}", &file);
+    }
+}
