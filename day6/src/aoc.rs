d8362f84d1f45e02420a77a31f19824a27a25e99
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2022-12-12 18:53:42 +0100

    [aoc-06] Implementation for part 2

--- day6/src/aoc.rs
+++ day6/src/aoc.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -14,19 +15,31 @@ pub fn run(file: String) {
     if let Ok(lines) = read_lines(&file) {
         for line in lines {
             if let Ok(line) = line {
-                let wnd = line
+                let sop = line
                     .as_bytes()
                     .windows(4)
                     .position(|x| {
-                        x[0] != x[1]
-                            && x[0] != x[2]
-                            && x[0] != x[3]
-                            && x[1] != x[2]
-                            && x[1] != x[3]
-                            && x[2] != x[3]
+                        let map: HashSet<&u8> = HashSet::from_iter(x);
+                        map.len() == 4
                     })
-                    .expect("Index not found");
-                println!("Found first marker after character: {}", wnd + 4);
+                    .expect("Index not found")
+                    + 4;
+                //println!("Found first Start Of Packet marker after character: {}", sop);
+
+                let som = line[sop..]
+                    .as_bytes()
+                    .windows(14)
+                    .position(|x| {
+                        let map: HashSet<&u8> = HashSet::from_iter(x);
+                        map.len() == 14
+                    })
+                    .expect("Index not found")
+                    + 14
+                    + sop;
+                println!(
+                    "Found first Start Of Message marker after character: {}",
+                    som
+                );
             }
         }
     } else {
